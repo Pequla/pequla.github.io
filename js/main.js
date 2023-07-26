f4a635280d24295908e0cdd376a260e11a98f97b
Petar Kresoja <petarkresoja5@gmail.com>
2023-07-26 12:53:49 +0200

    New website upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.getElementById('github').addEventListener('click', function () {
+    window.open('https://github.com/Pequla', '_blank')
+})
+
+document.getElementById('steam').addEventListener('click', function () {
+    window.open('https://steamcommunity.com/id/pequla', '_blank')
+})
+
+document.getElementById('discord').addEventListener('click', function () {
+    window.open('https://discordapp.com/users/358236836113547265', '_blank')
+})
